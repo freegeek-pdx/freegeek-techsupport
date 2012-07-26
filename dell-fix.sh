98d6813fea31415d5f94cb9e6c5a026a91adb318
paulm <paulm@freegeek.org>
2012-07-26 11:04:06 -0700

    modified:   dell-fix.sh 	new file:   fix-fstab 	new file:   mount-ntfs

--- dell-fix.sh
+++ dell-fix.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-apt-add-repository ppa:glasen/855gm-fix
-apt-add-repository ppa:brian-rogers/graphics-fixes
-apt-add-repository ppa:glasen/intel-driver
-aptitude update
-aptitude install linux 855gm-fix-dkms
-aptitude dist-upgrade
 
+
+intel855(){
+apt-add-repository ppa:glasen/intel-driver
+apt-get update
+apt-get upgrade
+add-apt-repository ppa:glasen/855gm-fix
+apt-get update
+apt-get install linux 855gm-fix-dkms
+}
+
+reenable_kms(){
+	echo options i915 modeset=1 | tee /etc/modprobe.d/i915-kms.conf
+	update-initramfs -u
+}
+
+disable_dri(){
 cat <<EOF >/etc/X11/xorg.conf
 Section "Module"
         Disable "dri"
         Disable "glx"
 EndSection
 EOF
+}
+
+
+# MAIN
+lspci | grep -q "855GM Integrated Graphics"
+855_match=$?
+
+if [[ $855_match -eq 0 ]]; then
+	intel855
+elif $(lspci | grep -e "8..GM Integrated Graphics") ; then 
+	reenable_kms
+else
+	echo "8XX  chipset not found"
+fi
