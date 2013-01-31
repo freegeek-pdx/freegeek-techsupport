33fe46a56013e5721659dd7feba584d410f072f1
paulm <paulm@freegeek.org>
2013-01-31 13:47:08 -0800

    typo fix

--- upgrade-ubuntu.sh
+++ upgrade-ubuntu.sh
@@ -245,7 +245,7 @@ elif [[ $second_run ]]; then
     # remove unity etc
 
     if [[ ! $no_remove ]];  then
-        big_ass_list_of_packages_to_remove="gnome-control-center gnome-font-viewer gnome-media gnome-menus gnome-nettool gnome-power-manager gnome-screenshot gnome-session gnome-session-canberra gnome-system-log gnome-system-monitor gnome-terminal gcalctool eog nautilus nuatilus-sendto unity unity-2d unity-greeter gnome-bluetooth gnome-disk-utility gnome-orca gnome-screensaver gnome-sudoku gnomine ubuntuone-client-gnome"
+        big_ass_list_of_packages_to_remove="gnome-control-center gnome-font-viewer gnome-media gnome-menus gnome-nettool gnome-power-manager gnome-screenshot gnome-session gnome-session-canberra gnome-system-log gnome-system-monitor gnome-terminal gcalctool eog nautilus nautilus-sendto unity unity-2d unity-greeter gnome-bluetooth gnome-disk-utility gnome-orca gnome-screensaver gnome-sudoku gnomine ubuntuone-client-gnome"
 
         for package in $big_ass_list_of_packages_to_remove; do
            if  dpkg-query  -l  bash  2>/dev/null  | grep -q ^.i; then
